e9d566107264a702f5df4fb8fce40ca2afef5ae2
Andrey <the.withered.fern@gmail.com>
2018-10-01 20:31:46 +0300

    set container state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,20 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      salad: 2,
+      bacon: 1,
+      cheese: 2,
+      meat: 1
+    }
+  };
+
   render(){
     return (
       <Aux>
-        <Burger/>
+        <Burger ingredients={this.state.ingredients}/>
         <div>Controllers</div>
       </Aux>
     );
